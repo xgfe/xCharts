a2cf624e8d76b72564cb595facad3107f3f59d8f
scliuyang <mzefibp@163.com>
2016-03-08 15:07:02 +0800

    axis 重构

--- js/components/axis.js
+++ js/components/axis.js
@@ -1,15 +1,20 @@
 /**
  * xCharts.axis
  * 坐标系绘制函数
+ * TODO 坐标轴边界动态计算
  */
 (function (window) {
     var xCharts = window.xCharts;
     var utils = xCharts.utils;
     var d3 = window.d3;
-    utils.inherits(axis, xCharts.components['Component']);
-    xCharts.components.extend({axis: axis})
+    var components = xCharts.components;
+    utils.inherits(axis, components['Component']);
+    components.extend({axis: axis});
 
     function axis(messageCenter, config, type) {
+        /**
+         * 这里跟其他组件不一样，即使用户不愿意显示坐标轴，也必须初始化(init)，不然其他图表会拿不到比例尺导致绘图失败
+         */
         xCharts.components['Component'].call(this, messageCenter, config, type);
     }
 
@@ -21,26 +26,50 @@
             this.axisConfig = config[type];
             this.width = messageCenter.originalWidth - messageCenter.margin.left - messageCenter.margin.right; //计算剩余容器宽
             this.height = messageCenter.originalHeight - messageCenter.margin.top - messageCenter.margin.bottom;//计算剩余容器高
+
+            // 设置
             this.range = this.isXAxis ? [0, this.width] : [this.height, 0];
             this.series = series;
+
+
+            var scales = [];
+            for (var i = 0; i < this.axisConfig.length; i++) {
+                // 计算比例尺scale
+                scales[i] = axisScale(this.axisConfig[i], i, this.range, this);
+
+                // 合并默认config
+                this.axisConfig[i] = utils.merage(defaultConfig(type),  this.axisConfig[i]);
+            }
+
+            this.messageCenter[this.type + 'Scale'] = scales;
+            this.scales = scales;
+
         },
         render: function (ease, durationTime) {
-            this.messageCenter[this.type + 'Scale'] = [];
             var type = this.type;
-            for (var i = 0, a; a = this.axisConfig[i]; i++) {
-                var scale = this.__axis(a, i); //每个坐标轴都会生成一个scale
-                this.messageCenter[this.type + 'Scale'][i] = scale; //通过messageCenter将scale提供给其他组件使用
-                a = utils.merage(defaultConfig(type), a); //给予默认配置
-                this.axisConfig[i]=a; //为了tooltip时，能获取到格式化函数
-                //坐标轴函数
+            var scales = this.scales;
 
-                if(!a.show) return true; //不显示坐标
+            for (var i = 0, config; config = this.axisConfig[i]; i++) {
 
-               var axis = d3.svg.axis().scale(scale).orient(a.position).tickFormat(a.tickFormat);
+                if (!config.show) break; //不显示坐标
+
+                var scale = scales[i];
+
+                // d3内置函数,生成axis
+                var axis = d3.svg.axis()
+                    .scale(scale)
+                    .orient(config.position)
+                    .tickFormat(config.tickFormat);
+
                 //添加<g>
-                var axisGroup = this.main.selectAll(".xc-axis." + type + '-' + i).data([a]);
-                axisGroup.enter().append('g').attr('class', 'xc-axis ' + type + '-' + i).attr('fill', 'none').attr('stroke', '#000');
-                axisGroup.attr('transform', this.__translate(a))
+                var axisGroup = this.main.selectAll(".xc-axis." + type + '-' + i).data([config]);
+
+                axisGroup.enter().append('g')
+                    .attr('class', 'xc-axis ' + type + '-' + i)
+                    .attr('fill', 'none')
+                    .attr('stroke', '#000');
+
+                axisGroup.attr('transform', translate.call(this, config))
                     .transition()
                     .ease(ease)
                     .duration(durationTime)
@@ -49,28 +78,28 @@
             }
         },
         ready: function () {
-            //DONE tooltip完成后再写
-            this.__tooltipReady();
-            this.__lengendReady();
+
+            this._tooltipReady();
+            this._lengendReady();
         },
-        __tooltipReady: function () {
+        _tooltipReady: function () {
             //有些情况不需要加载tooltip事件
-            if (!this.config.tooltip ||!this.config.tooltip.show ||this.config.tooltip.trigger !== 'axis' || this.type=='yAxis') return;
+            if (!this.config.tooltip || this.config.tooltip.show===false || this.config.tooltip.trigger !== 'axis' || this.type == 'yAxis') return;
 
             //默认已经是单x轴,且type！=value
 
             var axis = this.axisConfig[0];
             this.on('tooltipSectionChange.axis', function (sectionNumber, callback) {
-                var data =axis.data[sectionNumber];
-                var html =axis.formatter(data);
+                var data = axis.data[sectionNumber];
+                var html = axis.formatter(data);
 
                 callback(html);
             });
 
         },
-        __lengendReady: function () {
+        _lengendReady: function () {
             var _this = this;
-            this.on('legendClick.'+_this.type, function (nameList) {
+            this.on('legendClick.' + _this.type, function (nameList) {
                 var series = _this.series;
                 series.forEach(function (serie) {
                     var serieName = serie.name;
@@ -86,125 +115,128 @@
                 _this.init(_this.messageCenter, _this.config, _this.type, series);
                 _this.render('linear', 0);
             });
-        },
-        __translate: function (singleConfig, type) {
-            var position = singleConfig.position;
-            var xy = [0, 0];
-
-            if (position == 'right')
-                xy = [this.width, 0];
-            else if (position == 'bottom')
-                xy = [0, this.height];
-            return 'translate(' + xy + ')';
-        },
-        __axis: function (singleConfig, idx) {
-            var axisClass = this['__' + singleConfig.type + 'Axis'];
-            if (!axisClass) {
-                console.error('axis[%d].type = "%s" is not supported', idx, singleConfig.type);
-                return;
-            }
-
-            return axisClass.call(this, singleConfig, idx);
-
-        },
-        __categoryAxis: function (singleConfig, idx) {
-            var range = this.range;
-
-            if (!singleConfig.data) {
-                console.error('axis[%d].data is not defined!', idx);
-                return;
-            }
-
-            if(isBar(this.config.series)){
-                var scale = d3.scale.ordinal()
-                    .domain(singleConfig.data)
-                    .rangeRoundBands(range,0,0.1);
-
-            }else{
-                var scale = d3.scale.ordinal()
-                    .domain(singleConfig.data)
-                    .rangeRoundPoints(range);
-            }
-
-            return scale;
-
-        },
-        __valueAxis: function (singleConfig, idx) {
-            // TODO 最大最小值，多一格出来，否则气泡图容易出界
-            var series = this.series;
-            var type = this.type;
-            var range = this.range;
-            //默认指向index=0的坐标轴
-            series.map(function (serie) {
-                if (serie[type + 'Index'] == null) {
-                    serie[type + 'Index'] = 0;
-                }
-            });
-
-            var values = [], domain = [];
-            for (var k in axisSeries) {
-                if (axisSeries.hasOwnProperty(k)) {
-                    var value = axisSeries[k](series, type, idx);
-                    if (value) {
-                        values.push(value);
-                    }
-                }
-
-            }
-            domain[0] = d3.min(values, function (value) {
-                return value[0]
-            });
-            domain[1] = d3.max(values, function (value) {
-                return value[1]
-            });
-            //用户手动控制最大最小值
-            if (domain[0] > singleConfig.minValue) {
-                domain[0] = singleConfig.minValue;
-            }
-            if (domain[1] < singleConfig.maxValue) {
-                domain[1] = singleConfig.maxValue;
-            }
-
-            var scale = d3.scale.linear()
-                .domain(domain)
-                .range(range);
-            return scale;
-        },
-        __timeAxis: function (singleConfig, idx) {
-            var range = this.range;
-            var scale = d3.time.scale()
-                .domain(d3.extent(singleConfig.data, function (d) {
-                    return +new Date(d);
-                }))
-                .range(range);
-
-            //现在只考虑x坐标轴
-            if(this.isXAxis){
-                //取得series中data的长度，均分domain,保证singleConfig.data长度一致
-                var series = this.config.series;
-                var dataLength=1;
-                for(var i= 0,serie;serie=series[i++];){
-                    // 这里只支持折线图，暂不考虑其他
-                    var xIndex = serie.xAxisIndex==undefined?0:serie.xAxisIndex;
-                    if(serie.type=='line' && xIndex==idx){
-                        dataLength=serie.data.length-1;
-                        break;
-                    }
-                }
-
-                var domain = scale.domain(),
-                    timeDifference=domain[1]-domain[0], //最大最小时间差
-                    sectionTime = timeDifference/dataLength;//时间与时间之间的间隔
-
-                singleConfig.data=[+domain[0]];
-                for(var i=0;i<dataLength;i++){
-                    singleConfig.data[i+1]=+domain[0]+Math.round(sectionTime*(i+1),1);
-                }
-            }
-
-            return scale;
         }
     });
+
+
+    var axisCategory = {
+        value: valueAxis,
+        time: timeAxis,
+        category: categoryAxis
+    }
+
+    function axisScale(singleConfig, idx, range, ctx) {
+        var axisClass = axisCategory[singleConfig.type];
+        if (!axisClass) {
+            console.error('axis[%d].type = "%s" is not supported', idx, singleConfig.type);
+            return;
+        }
+
+        return axisClass.call(ctx, singleConfig, idx,range);
+
+    }
+
+
+    function categoryAxis(singleConfig, idx,range) {
+
+        if (!singleConfig.data) {
+            console.error('axis[%d].data is not defined!', idx);
+            return;
+        }
+
+        if (isBar(this.config.series)) {
+            var scale = d3.scale.ordinal()
+                .domain(singleConfig.data)
+                .rangeRoundBands(range, 0, 0.1);
+
+        } else {
+            var scale = d3.scale.ordinal()
+                .domain(singleConfig.data)
+                .rangeRoundPoints(range);
+        }
+
+        return scale;
+
+    }
+
+
+    function valueAxis(singleConfig, idx,range) {
+        // TODO 最大最小值，多一格出来，否则气泡图容易出界
+        var series = this.series;
+        var type = this.type;
+        //默认指向index=0的坐标轴
+        series.map(function (serie) {
+            if (serie[type + 'Index'] == null) {
+                serie[type + 'Index'] = 0;
+            }
+        });
+
+        var values = [], domain = [];
+        for (var k in axisSeries) {
+            if (axisSeries.hasOwnProperty(k)) {
+                var value = axisSeries[k](series, type, idx);
+                if (value) {
+                    values.push(value);
+                }
+            }
+
+        }
+        domain[0] = d3.min(values, function (value) {
+            return value[0]
+        });
+        domain[1] = d3.max(values, function (value) {
+            return value[1]
+        });
+        //用户手动控制最大最小值
+        if (domain[0] > singleConfig.minValue) {
+            domain[0] = singleConfig.minValue;
+        }
+        if (domain[1] < singleConfig.maxValue) {
+            domain[1] = singleConfig.maxValue;
+        }
+
+        var scale = d3.scale.linear()
+            .domain(domain)
+            .range(range);
+        return scale;
+    }
+
+    function timeAxis(singleConfig, idx,range) {
+
+        var scale = d3.time.scale()
+            .domain(d3.extent(singleConfig.data, function (d) {
+                return +new Date(d);
+            }))
+            .range(range);
+
+        //现在只考虑x坐标轴
+        if (this.isXAxis) {
+            //取得series中data的长度，均分domain,保证singleConfig.data长度一致
+            var series = this.config.series;
+            var dataLength = 1;
+            for (var i = 0, serie; serie = series[i++];) {
+                // 这里只支持折线图，暂不考虑其他
+                var xIndex = serie.xAxisIndex == undefined ? 0 : serie.xAxisIndex;
+                if (serie.type == 'line' && xIndex == idx) {
+                    dataLength = serie.data.length - 1;
+                    break;
+                }
+            }
+
+            var domain = scale.domain(),
+                timeDifference = domain[1] - domain[0], //最大最小时间差
+                sectionTime = timeDifference / dataLength;//时间与时间之间的间隔
+
+            singleConfig.data = [+domain[0]];
+            for (var i = 0; i < dataLength; i++) {
+                singleConfig.data[i + 1] = +domain[0] + Math.round(sectionTime * (i + 1), 1);
+            }
+        }
+
+        return scale;
+    }
+
+
     //因图表类型而异，取得对应的最大最小值
     //需对每个series指定了xAxisIndex 或者 yAxisIndex
     //@return 数组[min,max]
@@ -289,10 +321,26 @@
         }
     }
 
+    /**
+     * 根据作为位置返回需要偏移的坐标量
+     * @param config
+     * @returns {string} translate(0,0)
+     */
+    function translate(config) {
+        var position = config.position;
+        var xy = [0, 0];
 
-    function isBar(series){
-        for(var i= 0,s;s=series[i++];)
-            if(s.type==='bar')
+        if (position == 'right')
+            xy = [this.width, 0];
+        else if (position == 'bottom')
+            xy = [0, this.height];
+        return 'translate(' + xy + ')';
+    }
+
+
+    function isBar(series) {
+        for (var i = 0, s; s = series[i++];)
+            if (s.type === 'bar')
                 return true;
 
         return false;
@@ -353,8 +401,8 @@
              *  }
              *  @default 调用tickFormat进行处理,两边包裹<p>标签
              */
-            formatter: function(value){
-                return "<p>"+this.tickFormat(value)+"</p>";
+            formatter: function (value) {
+                return "<p>" + this.tickFormat(value) + "</p>";
             },
             /**
              * @var position
@@ -394,7 +442,7 @@
              * @description
              * 当不需要显示坐标轴时，可以关掉这个选项
              */
-            show:true
+            show: true
         }
         return axis;
     }
