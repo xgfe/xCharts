625f9ad05e2d05e28c435e2644b08276cba0a800
scliuyang <mzefibp@163.com>
2016-04-21 17:09:07 +0800

    chore(gulp): Use gulp to merge compression

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,52 @@
  * Describe
  */
 'use strict"'
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+var config = require('./package.json');
+var header = require('gulp-header');
+
+var uglifyOptions = {}
+
+var banner=['/**',
+    ' * <%= pkg.name %> - <%= pkg.description %>',
+    ' * @version v<%= pkg.version %>',
+    '*/',
+''].join('\n');
+
+var production = process.env.NODE_ENV === 'production';
+
+gulp.task('build-js', function () {
+    var pipe = gulp.src([
+        './src/xCharts.js',
+        './src/utils/utils.js',
+        './src/components/Component.js',
+        './src/components/*.js',
+        './src/charts/Chart.js',
+        './src/charts/*.js'
+    ]).pipe(concat('xCharts.js'))
+
+    if (!production) {
+        pipe.pipe(sourcemaps.init())
+    }
+
+    pipe.pipe(uglify(uglifyOptions))
+    if (!production) {
+        pipe.pipe(sourcemaps.write('./maps'))
+    }
+    pipe.pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(header(banner,{pkg:config}))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('build-css', function () {
+    return gulp.src('./src/css/xCharts.css')
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('build',['build-js','build-css']);
