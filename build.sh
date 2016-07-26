231e72a596e3d113000272a60af9253eef948595
陈五百 <chenwubai.cx@gmail.com>
2016-07-26 11:45:32 +0800

    docs(.gitignore, .npmignore): modify .gitignore and .npmignore

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm -rf dist/ && mkdir -p dist/ &&
-browserify src/xCharts.js src/utils/*.js src/components/*.js src/charts/*.js  -o dist/xCharts.js &&
-cat src/css/*.css > dist/xCharts.css
